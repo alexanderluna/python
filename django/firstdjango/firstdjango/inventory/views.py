8e6cd93cfe21a8336a1736c94e561b4d8cabb24d
Mycroft1891 <mycroft1891@gmail.com>
2018-03-30 22:52:37 +0200

    add django project

---
+++ django/firstdjango/firstdjango/inventory/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts   import render
+from django.http        import Http404
+from inventory.models   import Item
+
+def index(req):
+    items = Item.objects.exclude(amount=4)
+    return render(req, 'inventory/index.html', { 'items': items, })
+
+def item_detail(req, id):
+    try:
+        item = Item.objects.get(id=id)
+    except Item.DoesNotExist:
+        raise Http404('This item does not exist')
+    return render(req, 'inventory/show.html', { 'item': item, })
