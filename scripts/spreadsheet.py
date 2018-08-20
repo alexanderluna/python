352eed4ee5ecec059cb5d80ffe346b861507232f
Mycroft1891 <mycroft1891@gmail.com>
2018-08-20 15:14:21 +0200

    Update: comply with python style guide

--- scripts/spreadsheet.py
+++ scripts/spreadsheet.py
@@ -1,11 +1,11 @@
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+from oauth2client.service_account import ServiceAccountCredentials as ServiceA
 import pprint
 
 # use creds to create a client to interact with the Google Drive API
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
+creds = ServiceA.from_json_keyfile_name('credentials.json', scope)
 client = gspread.authorize(creds)
 
 # Find a workbook by name and open the first sheet
