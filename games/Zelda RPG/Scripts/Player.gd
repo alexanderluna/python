ecdfc75d5d85ec49b33e9f4cb664cb19c4ea7ca5
Mycroft1891 <mycroft1891@gmail.com>
2020-01-27 20:07:46 +0100

    Update: make player movement more realistic

--- games/Zelda RPG/Scripts/Player.gd
+++ games/Zelda RPG/Scripts/Player.gd
@@ -1,15 +1,30 @@
 extends KinematicBody2D
 
+const ACCELERATION = 50
+const MAX_SPEED = 100
+const FRICTION = 50
+
 var velocity = Vector2.ZERO
 
-func _physics_process(_delta):
-	var input_vector = Vector2.ZERO
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	
-	if input_vector != Vector2.ZERO:
-		velocity = input_vector
-	else:
-		velocity = Vector2.ZERO
 
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector = get_player_input()
+	velocity = calculate_velocity(input_vector, delta)
 	move_and_collide(velocity)
+
+
+func get_player_input():
+	return Vector2(
+		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
+		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	).normalized()
+
+
+func calculate_velocity(vector, delta):
+	if vector != Vector2.ZERO:
+		velocity += vector * ACCELERATION * delta
+		velocity = velocity.clamped(MAX_SPEED * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	return velocity
