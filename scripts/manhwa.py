23157b35d00202a189f2b0539b0f82aea9f09749
Alexander Luna <alexanderluna1891@gmail.com>
2020-11-18 18:25:50 +0100

    Fix: parsing errors and improve scrapping to resume if interrupted

--- scripts/manhwa.py
+++ scripts/manhwa.py
@@ -1,9 +1,9 @@
-#!/usr/local/bin/python3
+#!/usr/bin/env python
 
-import os
 import argparse
 from time import sleep
 from requests import get
+from os import path, mkdir
 from bs4 import BeautifulSoup as bs
 
 
@@ -13,48 +13,56 @@ def main(link):
     download_chapters(chapters)
 
 
-def fetch_chapters_from(html):
+def fetch_chapters_from(html) -> list[str]:
     print("=> Fetching chapters")
     chapters = list(map(lambda chapter: chapter["href"],
-                        html.select(".wp-manga-chapter > a")))
+                        html.select("ul.list-chapters > a")))
     chapters.reverse()
     return chapters
 
 
-def download_chapters(chapters):
+def download_chapters(chapters: list[str]) -> None:
     print("=> downloading {} chapters".format(len(chapters)))
-    for chapter in chapters:
-        folder = make_folder_for(chapter)
+    for current_chapter, chapter in enumerate(chapters):
+        folder = make_folder_for(current_chapter)
         if folder:
             images = fetch_images_from(chapter)
             download(images, folder)
-            sleep(30)
+            sleep(10)
 
 
-def make_folder_for(chapter):
-    folder_name = "".join(chapter.split("/")[-2:-1])
-    if not os.path.exists(folder_name):
-        print("\n=> Downloading {}".format(folder_name))
-        os.mkdir(folder_name)
-        return folder_name
-    return None
+def make_folder_for(chapter: int) -> str | None:
+    folder_name = str(chapter).zfill(3)
+    if path.exists(folder_name) or path.exists(f"{folder_name}.pdf"):
+        return None
+    print(f"\n=> Downloading {folder_name}")
+    mkdir(folder_name)
+    return folder_name
 
 
-def fetch_images_from(chapter):
+def fetch_images_from(chapter: str) -> list[str]:
     print("=> Parsing Images")
     parsed_chapter = bs(get(chapter).content, 'html.parser')
-    return list(map(lambda img: "".join(img["src"].strip().split("?")[0]),
-                    parsed_chapter.select(".wp-manga-chapter-img")))
+    return list(map(lambda img: img["data-src"],
+                    parsed_chapter.select("#chapter-content > img")))
 
 
-def download(images, folder):
-    print("=> Downloading {} images".format(len(images)))
-    for image in images:
-        file_name = "".join(image.split("/")[-1:])
-        file_path = "{}/{}".format(folder, file_name)
-        image_file = get(image)
-        open(file_path, "wb").write(image_file.content)
-        print("* {}".format(file_path))
+def download(images: list[str], folder: str) -> None:
+    print(f"=> Downloading {len(images)} images")
+    for index, image in enumerate(images):
+        file_path = generate_file_path(index, image, folder)
+        save_image(file_path, image)
+
+
+def generate_file_path(index: int, image: str, folder: str) -> str:
+    name = "".join(image.split("/")[-1:])
+    return f"{folder}/{str(index).zfill(3)} {name}"
+
+
+def save_image(file_path: str, image: str) -> None:
+    image_file = get(image)
+    open(file_path, "wb").write(image_file.content)
+    print("* {}".format(file_path))
 
 
 if __name__ == "__main__":
